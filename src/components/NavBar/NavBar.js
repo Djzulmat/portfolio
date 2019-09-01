3e512c4ff6dd8d89d2193a07ba9611868f58a91b
zafarkaharov <zkaharov@gmail.com>
2019-09-01 16:02:58 -0700

    Navbar design

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,23 +1,75 @@
-import React from 'react';
-import './NavBar.css'
+import React from "react";
+import { Link } from "react-scroll";
 
-
+import "./NavBar.css";
 
 const NavBar = () => {
-    return (
-       <nav>
-        <a className="hamburger" href=""><i className="fa fa-bars"></i></a>
-        <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#skills">Skills</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#contact">Contact</a></li>
-        </ul>
-        </nav>
-    )
-
+  return (
+    <nav className="navbar navbar-expand-md fixed-top ">
+      <ul className="nav">
+        <li class="nav-item">
+          <Link
+            activeClass="active"
+            className="home nav-link"
+            to="home"
+            spy={true}
+            smooth={true}
+            duration={500}
+          >
+            Home
+          </Link>
+        </li>
+        <li class="nav-item">
+          <Link
+            activeClass="active"
+            className="about nav-link"
+            to="about"
+            spy={true}
+            smooth={true}
+            duration={500}
+          >
+            About
+          </Link>
+        </li>
+        <li class="nav-item">
+          <Link
+            activeClass="active"
+            className="skills nav-link"
+            to="skills"
+            spy={true}
+            smooth={true}
+            duration={500}
+          >
+            Skills
+          </Link>
+        </li>
+        <li class="nav-item">
+          <Link
+            activeClass="active"
+            className="projects nav-link"
+            to="projects"
+            spy={true}
+            smooth={true}
+            duration={500}
+          >
+            Projects
+          </Link>
+        </li>
+        <li class="nav-item">
+          <Link
+            activeClass="active"
+            className="contact nav-link"
+            to="contact"
+            spy={true}
+            smooth={true}
+            duration={500}
+          >
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default NavBar;
-
